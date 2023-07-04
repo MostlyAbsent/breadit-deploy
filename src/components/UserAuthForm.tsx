04955cf93b720b0f62adc14fab0852d8e835fe6a
Jacob Doran <j.doran.admin@gmail.com>
2023-07-04 18:18:29 +0800

    Added Button UI for user auth

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Button } from "./ui/Button";
 import { classNameMerge } from "@/lib/utils";
+import { Icons } from "./icons";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -10,7 +11,13 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
 			className={classNameMerge("flex justify-center", className)}
 			{...props}
 		>
-			<Button size="sm" className="w-full">
+			<Button
+				onClick={loginWithGoogle}
+				isLoading={isLoading}
+				size="sm"
+				className="w-full"
+			>
+				{isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
 				Google
 			</Button>
 		</div>
